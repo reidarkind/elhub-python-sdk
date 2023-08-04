6d09800e194cc48ae1ad6cce11ba7ea33d548208
Reidar <reidar.kind@sintef.no>
2023-08-04 14:11:12 +0200

    Added tests to masterdata requests.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -7,6 +7,7 @@ from elhub_sdk.client import APIClient, ElHubEnvironment, ElHubService
 from elhub_sdk.consumption import poll_consumption, request_consumption
 from elhub_sdk.enrollment import get_meter_characteristics
 from elhub_sdk.acknolwedgment import acknowledge_poll
+from elhub_sdk.masterdata import request_masterdata_customer, request_masterdata_metering_point
 from elhub_sdk.enums import THIRD_PARTY_ACTION
 from elhub_sdk.settings import CERT_FILE, KEY_FILE
 from elhub_sdk.third_party import request_action
@@ -169,3 +170,99 @@ def test_acknowledge_poll_market_processes():
         original_business_document_reference=original_business_document_reference,
     )
     assert response != False
+
+
+@pytest.mark.integrationtest
+def test_request_marketdata_customer():
+    """
+    Query requesting consumption for a meter
+    Returns:
+
+    """
+    meter_identificator = "707057500057411768"
+    snapshotdate = datetime.datetime(2022, 5, 1)
+
+    client, history = APIClient.get_client(
+        environment=ElHubEnvironment.TEST,
+        service=ElHubService.QUERY,
+        key_file=KEY_FILE,
+        cert_file=CERT_FILE,
+    )
+
+    response = request_masterdata_customer(
+        client, history, meter_identificator, sender_gsn=THIRD_PARTY_GSN_EXA, snapshotdate=snapshotdate
+    )
+    assert response != False
+
+
+@pytest.mark.integrationtest
+def test_request_marketdata_metering_point_w_snapshot():
+    """
+    Query requesting consumption for a meter
+    Returns:
+
+    """
+    meter_identificator = "707057500057411768"
+    snapshotdate = datetime.datetime(2022, 5, 1)
+
+    client, history = APIClient.get_client(
+        environment=ElHubEnvironment.TEST,
+        service=ElHubService.QUERY,
+        key_file=KEY_FILE,
+        cert_file=CERT_FILE,
+    )
+
+    response = request_masterdata_metering_point(
+        client, history, meter_identificator, sender_gsn=THIRD_PARTY_GSN_EXA, snapshotdate=snapshotdate
+    )
+    assert response != False
+
+
+@pytest.mark.integrationtest
+def test_request_marketdata_metering_point_w_start_end():
+    """
+    Query requesting consumption for a meter
+    Returns:
+
+    """
+    meter_identificator = "707057500057411768"
+    start = datetime.datetime(2022, 5, 1)
+    end = start + datetime.timedelta(days=7)
+
+    client, history = APIClient.get_client(
+        environment=ElHubEnvironment.TEST,
+        service=ElHubService.QUERY,
+        key_file=KEY_FILE,
+        cert_file=CERT_FILE,
+    )
+
+    response = request_masterdata_metering_point(
+        client, history, meter_identificator, sender_gsn=THIRD_PARTY_GSN_EXA, start=start, end=end
+    )
+    assert response != False
+
+@pytest.mark.integrationtest
+def test_request_marketdata_metering_point_w_snapshot_start_end():
+    """
+    Query requesting consumption for a meter
+    Returns:
+
+    """
+    meter_identificator = "707057500057411768"
+    start = datetime.datetime(2022, 5, 1)
+    end = start + datetime.timedelta(days=7)
+    snapshotdate = start
+
+    client, history = APIClient.get_client(
+        environment=ElHubEnvironment.TEST,
+        service=ElHubService.QUERY,
+        key_file=KEY_FILE,
+        cert_file=CERT_FILE,
+    )
+
+    response = request_masterdata_metering_point(
+        client, history, meter_identificator, sender_gsn=THIRD_PARTY_GSN_EXA, snapshotdate=snapshotdate, start=start, end=end
+    )
+    assert response != False
+
+
